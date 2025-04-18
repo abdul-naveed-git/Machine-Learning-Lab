afc429a4aed77a98d714e75c4154b18045cbceb5
abdul-naveed-git <skabdulnaveed0675@gmail.com>
2025-04-18 17:27:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import pickle
 
 # importing model
 model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+
 
 # creating flask app
 app = Flask(__name__)
@@ -29,9 +28,8 @@ def predict():
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
     single_pred = np.array(feature_list).reshape(1, -1)
 
-    scaled_features = ms.transform(single_pred)
-    final_features = sc.transform(scaled_features)
-    prediction = model.predict(final_features)
+   
+    prediction = model.predict(single_pred)
 
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
